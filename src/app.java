411090ef75f1283210ba3143cf4631e6ef928aac
Zapp <Zapp@Zapps_laptop.linux.bzap.com>
2015-03-02 20:22:01 -0500

    IST240 project first commit

---
+++ src/app.java
@@ -0,0 +1,11 @@
+
+public class app {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
